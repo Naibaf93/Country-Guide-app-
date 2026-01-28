6bcd12d3a38f85461af3080b90af6667ed8db5e4
Fabián Huertas <miloh9318@hotmail.com>
2026-01-27 19:13:50 -0500

    agregando la api en finalURL

--- js/app.js
+++ js/app.js
@@ -2,5 +2,7 @@ let searchBtn = document.getElementById("search-btn");
 let countryInput = document.getElementById("country-input");
 
 searchBtn.addEventListener("click", () => {
-    
+    let countryName = countryInput.value;
+    let finalURL = `https://restcountries.com/v3.1/name/${countryName}?fullText=true`;
+    console.log(finalURL);
 });
