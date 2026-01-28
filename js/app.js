af085a7242dc6141d8d41d838720bfcff2ef3769
Fabián Huertas <miloh9318@hotmail.com>
2026-01-27 19:19:39 -0500

    haciendo el fetch

--- js/app.js
+++ js/app.js
@@ -5,4 +5,9 @@ searchBtn.addEventListener("click", () => {
     let countryName = countryInput.value;
     let finalURL = `https://restcountries.com/v3.1/name/${countryName}?fullText=true`;
     console.log(finalURL);
+    fetch(finalURL)
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(data)
+        });
 });
