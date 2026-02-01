eaa00709a3bf199ff292e4597d45fed0f3ff3481
Fabián Huertas <miloh9318@hotmail.com>
2026-01-31 20:58:53 -0500

    inyectando la bandera en html

--- js/app.js
+++ js/app.js
@@ -14,6 +14,8 @@ searchBtn.addEventListener("click", () => {
             console.log(data[0].name.common);
             console.log(data[0].continents[0]);
             console.log(Object.keys(data[0].currencies)[0]);
-            console.log(data[0].currencies[Object.keys(data[0].currencies)].name)
+            console.log(data[0].currencies[Object.keys(data[0].currencies)].name);
+            console.log(Object.values(data[0].languages).toString().split(",").join(", "));
+            result.innerHTML = `<img src="${data[0].flags.svg}" class="flag-img">`;
         });
 });
