bf8a49811dfb4bd7c5c0fe371aa4d9e3f32bc480
Fabián Huertas <miloh9318@hotmail.com>
2026-01-31 21:01:25 -0500

    insertando h2 al html

--- js/app.js
+++ js/app.js
@@ -16,6 +16,9 @@ searchBtn.addEventListener("click", () => {
             console.log(Object.keys(data[0].currencies)[0]);
             console.log(data[0].currencies[Object.keys(data[0].currencies)].name);
             console.log(Object.values(data[0].languages).toString().split(",").join(", "));
-            result.innerHTML = `<img src="${data[0].flags.svg}" class="flag-img">`;
+            result.innerHTML = `
+                <img src="${data[0].flags.svg}" class="flag-img">
+                <h2>${data[0].name.common}</h2>
+                `;
         });
 });
